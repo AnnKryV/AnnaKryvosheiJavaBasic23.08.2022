18ad924c2b5236bb0a4d6a976a57821a64f50564
Ann Kruvoshei <coolskeletonpapyrus37@gmail.com>
2022-08-29 13:49:11 +0300

    first commit

--- Main.java
+++ Main.java
@@ -1,22 +1,39 @@
-package com.hillel.kryvoshei.homework.homework3;
+package com.hillel.kryvoshei.homework.homework5;
 
 public class Main {
-    /*Паралелепіпед:
-Створити у новому класі main-метод;
-Створити три змінні для зберігання сторін паралелепіпеда;
-Порахувати обсяг у змінній volume;
-Вивести на екран змінну volume у вигляді:
-System.out.println("Об'єм паралелепіпеда = " + volume);
-Порахувати сумарну довжину всіх сторін у змінній length та вивести її на екран;
-*/
     public static void main(String[] args) {
-        int len = 15;
-        int h = 27;
-        int wid = 24;
+        /*За владу в стародавньому Китаї боролися дві династії: Лі та Мінь.
 
-        var volume = len * h * wid;
-        var length = (len + h + wid)*4;
-        System.out.println("Об'єм паралелепіпеда = " + volume);
-        System.out.println("Сумарна довжина всіх сторін:" + length);
+З кожного боку боролися 3 типи воїнів з такими показниками атаки:
+Лі:
+
+воїн - 13
+лучник - 24
+вершник - 46
+
+Мінь:
+
+воїн - 9
+лучник - 35
+вершник - 12
+Розрахувати: загальний показник атаки для обох династій, якщо відомо,
+що воїнів династії Лі кожного типу було 860, а кількість воїнів династії Мінь
+кожного типу в півтора рази більша.
+
+Вивести в консоль значення загальної атаки кожної армії;
+
+*/
+        int lisaber = 13*860;
+        int liarcher = 24*860;
+        int lirider = 46*860;
+
+        double minsaber = 9*860*1.5;
+        double minarcher = 35*860*1.5;
+        double minrider = 12*860*1.5;
+
+        double litotalattack = liarcher + lirider + lisaber;
+        double mintotalattack =  minarcher + minsaber + minrider;
+        System.out.println("Min total:"+ (int)mintotalattack);
+        System.out.println("Li total:"+ (int)litotalattack);
     }
 }
